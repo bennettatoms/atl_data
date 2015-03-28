cee6ba327f4a3fbcf591cf5aa48d4c4822953fe8
Bennett Adams <bennettadams46@gmail.com>
2015-03-27 22:36:46 -0400

    added housing permit csv file

--- client/app/scripts/controllers/movers.js
+++ client/app/scripts/controllers/movers.js
@@ -8,28 +8,11 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-.controller('MoversCtrl', ['$scope', 'MoversService', function ($scope, MoversService) {
+.controller('MoversCtrl', ['$scope', 'MoversService', function($scope, MoversService) {
 
-  $scope.movers = [];
-  // var i;
   function getMovers() {
     MoversService.getMovers().success(function(data) {
       $scope.movers = data;
-      // for (i=0; i < data.length; i++) {
-      //   var moversData = {
-      //   year:             data[i].year, 
-      //   originCodeState:  data[i].origin_code_state, 
-      //   originCodeCounty: data[i].origin_code_county,
-      //   originState:      data[i].origin_state,
-      //   originCounty:     data[i].origin_county,
-      //   destCodeState:    data[i].dest_code_state,
-      //   destCodeCounty:   data[i].dest_code_county,
-      //   destState:        data[i].dest_state,
-      //   destCounty:       data[i].dest_county, 
-      //   numMovers:        data[i].num_movers
-      //   };
-      // $scope.movers.push(moversData[i]);
-      // }
     })
     .error(function() {
       alert('GET: error');
