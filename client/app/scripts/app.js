2dc3f8c696a7f6d28524d8f376c6cec9dc910840
Bennett Adams <bennettadams46@gmail.com>
2015-04-03 16:55:09 -0400

    gist of main page working -- needs styling

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -23,33 +23,23 @@ angular
   .config(['$stateProvider', '$urlRouterProvider', 
         function ($stateProvider, $urlRouterProvider) {
 
-
   $stateProvider
     .state('home', {
       url: '/',
       templateUrl: 'views/main.html',
       controller: 'mainCtrl'
     })
-      // the default route when someone hits dashboard
+      // the default route when someone hits dashboard    <!-- endbower -->
+
     .state('home.migrations', {
-      url: '/migrations',
+      url: 'migrations',
       templateUrl: 'views/chord.html',
       controller: 'ChordCtrl'
     })
     .state('home.population', {
-      url: '/population',
+      url: 'population',
       templateUrl: 'views/population.html',
       controller: 'populationCtrl'
-    })
-    .state('home.development', {
-      url: '/development',
-      templateUrl: 'views/pie_chart.html',
-      controller: 'permitPieCtrl'
-    })
-    .state('home.bar', {
-      url: '/bar',
-      templateUrl: 'views/bldg_pmt.html',
-      controller: 'BldgPmtCtrl'
     });
  // default fall back route
     $urlRouterProvider.otherwise('/');
